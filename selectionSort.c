53ac7924bc4180a35192f35bd7981f8ba94153da
Nitish kamath <82676204+Nitish-Kamath@users.noreply.github.com>
2022-03-13 22:58:04 +0530

    Comments are added

--- selectionSort.c
+++ selectionSort.c
@@ -1,34 +1,53 @@
-#include<stdlib.h>
-#include<stdio.h>
+// Basically it does selection
+// for increasing order
+// we will get the minimum value location in main functin and replace that value with arr[k]
+// time complexity in all the case is O(n^2) and space complexity is O(1)
 
-int min(int a[],int k, int n)
+#include <stdlib.h>
+#include <stdio.h>
+
+int min(int arr[], int k, int n)
 {
-    int loc,min;
-    min = a[k];
-    loc = k;
-    for(int j = k+1;j<=n-1;j++)
+
+    // storing the starting element as min
+    int min = arr[k];
+
+    // and its posn as min posn
+    int loc = k;
+    for (int j = k + 1; j <= n; j++)
     {
-        if(min>a[j])
+        if (min > arr[j])
         {
-            min =a[j];
+            min = arr[j];
             loc = j;
         }
-        return loc;
     }
+    return loc;
 }
 int main()
 {
-    int a[] = {33,11,66,88,99,77,44,55,11};
+    // declaring and initialising array with 10 element
+    int arr[] = {32, 42, 40, 19, 29, 61, 78, 18, 35, 71};
+    // finding length of array in c
+    int len = sizeof(arr) / sizeof(int);
+
     int loc, k, temp;
-    for(k = 0;k<=8;k++)
+
+    // why 8 not 9 , bcz if n-1 element is sorted then all element is sorted
+    for (k = 0; k <= 8; k++)
     {
-        loc = min(a,k,10);
-        temp= a[k];
-        a[k]= a[loc];
-        a[loc] = temp;
+        // loc will get the position of minimum element
+        loc = min(arr, k, len - 1);
+        // now we will interchange the starter element with min element
+        temp = arr[k];
+        arr[k] = arr[loc];
+        arr[loc] = temp;
     }
-    for(k=0;k<=9;k++)
+
+    // for printing the sorted value
+
+    for (k = 0; k <= len - 1; k++)
     {
-        printf(" %d",a[k]);
+        printf(" %d", arr[k]);
     }
 }
