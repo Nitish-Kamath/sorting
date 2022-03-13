d6c4c5f6cf98c972d86451a6cb8bf891f64ffa47
Nitish kamath <82676204+Nitish-Kamath@users.noreply.github.com>
2022-03-13 22:33:55 +0530

    comments are added

--- insertionsort.c
+++ insertionsort.c
@@ -1,27 +1,48 @@
+// printing in Ascending order
+// worst case O(n^2)
+// average case O(n^2)
+// best case O(n)
+// space complexity O(1)
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertion_sort(int a[], int n)
+void insertion_sort(int arr[], int len)
 {
-    int temp;
-    int j;
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= len - 1; i++)
     {
-        temp = a[i];
-        for (j = i - 1; j >= 0 && temp < a[j]; j-- )
+        // temp will takes all element except first element
+        int temp = arr[i];
+
+        // arr[j] will takes element, starting from element prev to the temp to the starting element
+        for (int j = i - 1; j >= 0 && temp < arr[j]; j--)
         {
-            a[j + 1] = a[j];
+            // if next value  is less thant the previous value then next value  will be replaced by the prev one
+
+            // MASTER STEP
+            arr[j + 1] = arr[j];
+            // NOTE: after this step, NEXT VALUE AS WELL AS THE PREV VALUE HAVE THE SAME NUMERIC VALUE
         }
-        a[j + 1] = temp;
+        // j = -1
+        // arr[0] = temp;
+        // shifting temp value to the previous one-
+
+        // MASTER STEP
+        arr[j + 1] = temp;
     }
 }
 
 int main()
 {
-    int a[] = {34, 45, 11, 89, 55, 19, 22, 8, 40};
-    insertion_sort(a, 9);
-    for (int i = 0; i <= 8; i++)
+    int arr[] = {34, 45, 11, 89, 55, 19, 22, 8, 40, 31};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    // passing arr as well as its length, it turns it will sort all the element
+    insertion_sort(arr, len);
+
+    // element is sorted
+
+    // printing the sorted element
+    for (int i = 0; i <= len - 1; i++)
     {
-        printf("%d ", a[i]);
+        printf("%d ", arr[i]);
     }
 }
