c45f3b155cca0d7d0bb94d8f9171aed8dea4f009
Nitish kamath <82676204+Nitish-Kamath@users.noreply.github.com>
2022-03-13 23:20:02 +0530

    comments is being added

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,32 +1,50 @@
+// printing in ascending order
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble_sort(int a[], int n)
+void bubble_sort(int arr[], int len)
 {
-    int round, i, temp;
-    for (round = 1; round <= n - 1; round++)
+
+    // round + i = len
+    // at end of round 1, largest element will reach to end position
+    // at end of round 2, second largest element will reach to end-1 position
+    // at end of round 3, third largest element will reach to the end-2 position
+    // .
+    // .
+    // .
+
+// if total element is 5, then we have only 4 round 
+// in each round one element is assigned to its actual position
+// hence for 4 round, 4 element is sorted hence 5th element is sorted as a consequenece
+    for (int round = 1; round <= len - 1; round++)
     {
-        for (i = 0; i <= n - 1 - round; i++)
+        for (int i = 0; i <= len - 1 - round; i++)
         {
-            if (a[i] > a[i + 1])
+            // sending the max element to the last
+            // if prev greater than next
+            // then swap
+            if (arr[i] > arr[i + 1])
             {
-                temp = a[i];
-                a[i] = a[i + 1];
-                a[i + 1] = temp;
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
             }
         }
     }
 }
 
-
 int main()
 {
-    int a[] = {34, 15, 29, 8};
-    int i;
-    bubble_sort(a, 4);
-    for (int i = 0; i <= 3; i++)
+    // for testing purpose always take less size array
+
+    int arr[] = {13, 11, 19, 17};
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    bubble_sort(arr, len);
+
+    // element is sortd in ascending order
+    for (int i = 0; i <= len - 1; i++)
     {
-        printf(" %d", a[i]);
+        printf(" %d", arr[i]);
     }
 }
-
