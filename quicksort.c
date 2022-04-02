da1c8e5de65d3dc663226f388fd720b70c351062
Nitish kamath <82676204+Nitish-Kamath@users.noreply.github.com>
2022-04-02 14:50:40 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,57 +1,86 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void swap(int *a, int *b)
+// Java program for implementation of QuickSort
+class QuickSort
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+	/* This function takes last element as pivot,
+	places the pivot element at its correct
+	position in sorted array, and places all
+	smaller (smaller than pivot) to left of
+	pivot and all greater elements to right
+	of pivot */
+	public static int partition(int arr[], int low, int high)
+	{
+		int pivot = arr[high];
+		int i = (low-1); // index of smaller element
+		for (int j=low; j<high; j++)
+		{
+			// If current element is smaller than pivot
+			
+			if (arr[j] < pivot)
+			{
+				i++;
+
+				// swap arr[i] and arr[j]
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		int temp = arr[i+1];
+		arr[i+1] = arr[high];
+		arr[high] = temp;
+
+		for(int x:arr)
+		{
+			System.out.print(x + " ");
+		}
+		System.out.println();
+
+		return i+1;
+	}
+
+
+	/* The main function that implements QuickSort()
+	arr[] --> Array to be sorted,
+	low --> Starting index,
+	high --> Ending index */
+	public static void sort(int arr[], int low, int high)
+	{
+		if (low < high)
+		{
+			/* pi is partitioning index, arr[pi] is
+			now at right place */
+			int pi = partition(arr, low, high);
+
+			// Recursively sort elements befored 
+			// partition and after partition
+			sort(arr, low, pi-1);
+			sort(arr, pi+1, high);
+		}
+	}
+
+	/* A utility function to print array of size n */
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; i++)
+			System.out.print(arr[i]+" ");
+		System.out.println();
+	}
+
+	// Driver program
+	public static void main(String args[])
+	{
+		int arr[] = {10, 7, 8, 9, 1, 5};
+		int n = arr.length;
+
+		// QuickSort ob = new QuickSort();
+		// ob.sort(arr, 0, n-1);
+		sort(arr,0,n-1);
+
+		System.out.println("sorted array");
+		printArray(arr);
+	}
 }
-
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++)
-    {
-
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-int main()
-{
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, n - 1);
-    printf("Sorted Array:\n");
-    printArray(arr, n);
-    return 0;
-}
+ 
